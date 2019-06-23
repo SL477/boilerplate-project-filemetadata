020b0ca2a54af6a9b4fa55b8497698eb0ea49545
SL477 <fishwickt@gmail.com>
2019-06-23 17:51:34 +0100

    Initial commit
    File Meta data

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ var cors = require('cors');
 // require and use "multer"...
 
 var app = express();
+//multer
+var multer = require('multer');
+/*app.use(multer({
+  dest: 'uploads/'
+}));*/
+//var upload = multer({ dest: 'uploads/'});
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -16,6 +22,24 @@ app.get('/', function (req, res) {
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
+});
+
+//set storage
+var storage = multer.diskStorage({
+  destination: function (req, filename, cb) {
+    cb(null, 'uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.filename + '-' + Date.now())
+  }
+});
+var upload = multer({ storage: storage });
+
+//Setup the post request
+app.post("/api/fileanalyse", upload.single('upfile'), function (req, res) {
+  console.log(req.file);
+  //res.send(req.file);
+  res.json({"name": req.file.originalname, "type": req.file.mimetype, "size": req.file.size});
 });
 
 app.listen(process.env.PORT || 3000, function () {

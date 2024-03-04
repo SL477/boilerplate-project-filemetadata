a0dedf6e32490a03ea771fe0c4be0bba7f38b53b
Tom Fishwick <51207735+SL477@users.noreply.github.com>
2024-03-04 18:57:17 +0000

    Update dependencies (#1)
    * Updated the main dependancies
    
    * Remove files after use
    
    * Update README.md

--- server.js
+++ server.js
@@ -1,47 +1,63 @@
 'use strict';
 
-var express = require('express');
-var cors = require('cors');
+import express from 'express';
+import cors from 'cors';
+import fs from 'fs';
 
 // require and use "multer"...
-
-var app = express();
-//multer
-var multer = require('multer');
-/*app.use(multer({
-  dest: 'uploads/'
-}));*/
-//var upload = multer({ dest: 'uploads/'});
+const app = express();
+// multer
+import multer, { diskStorage } from 'multer';
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+    res.sendFile(process.cwd() + '/views/index.html');
 });
 
-//set storage
-var storage = multer.diskStorage({
-  destination: function (req, filename, cb) {
-    cb(null, 'uploads')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.filename + '-' + Date.now())
-  }
+app.get('/hello', function (req, res) {
+    res.json({ greetings: 'Hello, API' });
 });
-var upload = multer({ storage: storage });
 
-//Setup the post request
-app.post("/api/fileanalyse", upload.single('upfile'), function (req, res) {
-  console.log(req.file);
-  //res.send(req.file);
-  res.json({"name": req.file.originalname, "type": req.file.mimetype, "size": req.file.size});
+// set storage
+const storage = diskStorage({
+    destination: function (req, filename, cb) {
+        cb(null, 'uploads');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.filename + '-' + Date.now());
+    },
+});
+const upload = multer({ storage: storage });
+
+// Remove files after use
+app.use(function (req, res, next) {
+    const writeJson = res.json;
+    const writeJsonBound = writeJson.bind(res);
+    res.json = function (body) {
+        if (req.file) {
+            fs.unlink(req.file.path, function (err) {
+                if (err) {
+                    console.error(err);
+                }
+            });
+        }
+        writeJsonBound(body);
+    };
+    next();
+});
+
+// Setup the post request
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+    console.log(req.file);
+    res.json({
+        name: req.file.originalname,
+        type: req.file.mimetype,
+        size: req.file.size,
+    });
 });
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+    console.log('Node.js listening ...');
 });
